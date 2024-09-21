fdc986b66e3220b4994b96c162a3a7a037165292
unknown <u20231213568@usco.edu.co>
2024-09-21 09:35:15 -0500

    Add Role entity for user roles
    Table roles that is related to a intermediate userRoles for managing many roles for a single user

--- src/main/java/com/activegym/activegym/Repositories/RoleRepository.java
+++ src/main/java/com/activegym/activegym/Repositories/RoleRepository.java
@@ -1,4 +1,7 @@
 package com.activegym.activegym.Repositories;
 
-public interface RoleRepository {
+import com.activegym.activegym.Entities.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Integer> {
 }
