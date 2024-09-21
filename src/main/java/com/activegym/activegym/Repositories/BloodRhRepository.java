9874cdc9ad3754e435837fc1c4dc18406fa62f4c
unknown <u20231213568@usco.edu.co>
2024-09-21 09:45:41 -0500

    Fix: cast gender, blood data and eps from name to id using repositories

--- src/main/java/com/activegym/activegym/Repositories/BloodRhRepository.java
+++ src/main/java/com/activegym/activegym/Repositories/BloodRhRepository.java
@@ -3,5 +3,8 @@ package com.activegym.activegym.Repositories;
 import com.activegym.activegym.Entities.BloodRh;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface BloodRhRepository extends CrudRepository<BloodRh, Integer> {
+    Optional<BloodRh> findByBloodRh(String bloodRh);
 }
